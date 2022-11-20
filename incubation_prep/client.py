5b1d256c8c477273af9cd16b91ca12fb1c1af045
Tien-Cheng <tiencheng@protonmail.com>
2022-11-20 20:59:25 +0800

    [feat] Client for pipeline flow

--- incubation_prep/client.py
+++ incubation_prep/client.py
@@ -1,1 +1,17 @@
 # Client to stream incoming video to pipeline
+import click
+
+from .pipeline import Pipeline
+
+pipe = Pipeline()
+
+
+@click.command()
+@click.option("--video_path", required=True, type=click.Path(exists=True))
+@click.option("--output_path", required=True, type=click.Path())
+def stream_video(video_path, output_path):
+    pipe(video_path, output_path)
+
+
+if __name__ == "__main__":
+    stream_video()
