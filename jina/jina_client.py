f8051d233adfbd39acb2d20f3640807cc56e344e
Tien-Cheng <tiencheng@protonmail.com>
2022-12-01 09:09:22 +0800

    [fix] Memory leak from generator

--- jina/jina_client.py
+++ jina/jina_client.py
@@ -1,5 +1,6 @@
 from itertools import count
 from time import perf_counter, sleep
+from memory_profiler import profile
 
 import click
 import cv2
@@ -8,37 +9,47 @@ import numpy as np
 from jina import Client, Document
 
 
-
 class JinaClient:
     def __init__(self, host: str = "0.0.0.0", port: int = 4091):
         self.client = Client(host=host, port=port)
 
+    def create_stream(self, video_path):
+        self.cap = cv2.VideoCapture(video_path)
+
     @staticmethod
-    def split_frames(video_path: str):
-        cap = cv2.VideoCapture(video_path)
-        
+    def read_frames(cap: cv2.VideoCapture, stream_name):
         for frame_count in count():
             success, frame = cap.read()
             if not success:
                 break
-
-            sleep(1/30)
+            sleep(1 / 30)
             yield Document(
-                tensor=np.array(frame), tags={"frame_id": frame_count, "video_path": video_path, "output_stream" : "test2"}
+                tensor=np.array(frame),
+                tags={
+                    "frame_id": frame_count,
+                    "video_path": stream_name,
+                    "output_stream": "test2",
+                },
             )
 
+    
+    @profile
     def infer(self, video_path: str):
-        self.client.post(
-            on="/infer",
-            inputs=self.split_frames(video_path),
-            request_size=1,
-        )
+        for frame in self.read_frames(self.cap, video_path):
+            Document.generator_from_webcam
+            self.client.post(
+                on="/infer",
+                inputs=frame,
+                request_size=1,
+            )
+
 
 @click.command()
 @click.option("--video", "-v", type=click.Path(exists=True))
 def main(video):
     client = JinaClient()
     client.infer(video)
+
 
 if __name__ == "__main__":
     prev_frame_time = perf_counter()
