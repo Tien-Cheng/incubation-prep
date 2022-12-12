c9137666667bd8e6c9641d195c679b3f7eec402b
Tien-Cheng <tiencheng@protonmail.com>
2022-12-12 17:39:20 +0800

    [fix] Fix docker compose for pubsub

---
+++ incubation_prep/pubsub/create_topic.py
@@ -0,0 +1,19 @@
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
+from time import sleep
+
+client = KafkaAdminClient()
+topics = [
+    "dets",
+    "frames",
+    "tracks",
+    "out"
+]
+try:
+    client.delete_topics(topics)
+except:
+    pass
+sleep(5)
+client.create_topics(
+    [NewTopic(name, 1, 1) for name in topics]
+)
