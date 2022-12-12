9638a9a7e9aa05bc50d9b1477a7968ffb67ef254
Tien-Cheng <tiencheng@protonmail.com>
2022-12-12 10:59:06 +0800

    [dbg] Script to pull images onto cluster

---
+++ scripts/pull-pubsub-images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker pull tiencheng/pubsub-detector:1.1
+docker pull tiencheng/pubsub-tracker:1.1
+docker pull tiencheng/pubsub-save-frames:1.1
+docker pull tiencheng/pubsub-stream-output:1.1
