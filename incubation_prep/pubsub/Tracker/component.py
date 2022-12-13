e5a6517e08909103d6bae9dafe882680116ac2aa
Tien-Cheng <tiencheng@protonmail.com>
2022-12-13 11:17:53 +0800

    [fix] Update to check doc for redis

--- incubation_prep/pubsub/Tracker/component.py
+++ incubation_prep/pubsub/Tracker/component.py
@@ -238,9 +238,10 @@ class Component(ABC):
         return doc
 
     def _load_image_tensor_from_redis(self, doc: Document) -> Document:
-        image_key = doc.tags["redis"]
-        if self.rds.exists(image_key) != 0:
-            doc.blob = self.rds.get(image_key)
-            # Load bytes
-            return doc.convert_blob_to_image_tensor()
+        if "redis" in doc.tags:
+            image_key = doc.tags["redis"]
+            if self.rds.exists(image_key) != 0:
+                doc.blob = self.rds.get(image_key)
+                # Load bytes
+                return doc.convert_blob_to_image_tensor()
         return doc
